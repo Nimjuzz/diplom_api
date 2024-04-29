750f1c970663235d52a475a0ba902a4b59c2ae3c
Iliyas <ilyas.baygozy@mail.ru>
2024-04-29 22:06:03 +0500

    wl1

--- art_studio/settings.py
+++ art_studio/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from decouple import config
+from dotenv import load_dotenv
+
+load_dotenv()
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
