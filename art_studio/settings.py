5f448a731cd925b5e45d05cacfc4b786c496d6bc
Iliyas <ilyas.baygozy@mail.ru>
2024-05-01 12:46:24 +0500

    statsfixing2

--- art_studio/settings.py
+++ art_studio/settings.py
@@ -150,7 +150,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = "staticfiles/"
+STATIC_URL = "/app/staticfiles/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
